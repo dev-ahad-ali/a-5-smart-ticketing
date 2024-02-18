67711706d0ad426d0a55a180bac8afd02b9dca8e
Ahad Ali <ahadali.dev@gmail.com>
2024-02-18 21:58:11 +0600

    seat layout added

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Seat Letter Style -------------------
+function seatLetterStyle() {
+  const seats = document.getElementsByClassName("seat-letter");
+  for (const seat of seats) {
+    seat.classList.add(
+      "font-medium",
+      "text-lg",
+      "opacity-50",
+      "text-center",
+      "py-4"
+    );
+  }
+}
+seatLetterStyle();
+
+// Seat Serial Style -------------------
+function seatSerialStyle() {
+  const seats = document.getElementsByClassName("seat-serial");
+  for (const seat of seats) {
+    seat.classList.add(
+      "font-medium",
+      "text-lg",
+      "opacity-50",
+      "px-11",
+      "py-4",
+      "bg-[#F7F8F8]",
+      "rounded-xl",
+      "text-center"
+    );
+  }
+}
+seatSerialStyle();
